26b95bfffa7a578987947990a02398b6ccde7ef8
Ayush Singh <ayushsingh01042003@gmail.com>
2024-04-07 23:33:02 +0530

    Heap content

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -4,13 +4,18 @@ import java.util.PriorityQueue;
 
 public class Heap {
     public static void main(String[] args) {
-        // PriorityQueue<Integer> minHeap = new PriorityQueue<>(); default is minHeap
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>(); 
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         int[] arr = {10, 7, 12, 4, 0, 1};
 
         for(int i : arr) {
             maxHeap.add(i);
         }
-        System.out.println(maxHeap);
+        
+        // System.out.println(minHeap.peek());
+        System.out.println(maxHeap.peek());
     }
 }
+
+//peek gives min or max element of heap O(1)
+//add and remove is O(logn)
