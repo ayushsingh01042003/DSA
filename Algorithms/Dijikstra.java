618f5a37d88a9f4bbed06b105b0be59116be7326
Ayush Singh <ayushsingh01042003@gmail.com>
2025-02-23 21:54:45 +0530

    Update shortest path Dijikstra

--- Algorithms/Dijikstra.java
+++ Algorithms/Dijikstra.java
@@ -1,66 +1,43 @@
-import java.util.*;
+class Solution {
+    public Map<Integer, Integer> shortestPath(int n, List<List<Integer>> edges, int src) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<List<int[]>> adj = new ArrayList<>();
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-class Dijikstra {
-
-    public int[] shortestPath(int V, int E, int[][] edges) {
-        // Code here
-        List<List<Pair>> adj = new ArrayList<>();
-        
-        for(int i = 0; i < V; i++) {
+        for(int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        
-        for(int[] edge : edges) {
-            adj.get(edge[0]).add(new Pair(edge[1], edge[2]));
-        }
-        
-        Stack<Integer> stack = new Stack<>();
-        int[] distance = new int[V];
-        boolean[] visited = new boolean[V];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[0] = 0;
-        
-        for(int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                dfs(adj, visited, i, stack);
-            }
-        }
-        
-        while(!stack.isEmpty()) {
-            int topNode = stack.pop();
-            
-            if(distance[topNode] != Integer.MAX_VALUE) {
-                for(Pair pair : adj.get(topNode)) {
-                    distance[pair.node] = Math.min(
-                        distance[pair.node],
-                        distance[topNode] + pair.distance
-                    );
-                }
-            } else {
-                distance[topNode] = -1;
-            }
-        }
-        return distance;
-    }
-    
-    void dfs(List<List<Pair>> adj, boolean[] visited, int currNode, Stack<Integer> stack) {
-        visited[currNode] = true;
-        
-        for(Pair pair : adj.get(currNode)) {
-            if(!visited[pair.node]) {
-                dfs(adj, visited, pair.node, stack);
-            }
-        }
-        stack.push(currNode);
-    }
-}
 
-class Pair {
-    int node;
-    int distance;
-    
-    Pair(int node, int distance) {
-        this.node = node;
-        this.distance = distance;
-    }
+        for(List<Integer> edge : edges) {
+            adj.get(edge.get(0)).add(new int[]{ edge.get(1), edge.get(2) });
+        }
+
+        minHeap.add(new int[]{ 0, src });
+
+        while(!minHeap.isEmpty()) {
+            int[] top = minHeap.remove();
+            int topNode = top[1];
+            int topDist = top[0];
+
+            if(map.containsKey(topNode)) {
+                continue;
+            }
+
+            map.put(topNode, topDist);
+
+            for(int[] edge : adj.get(topNode)) {
+                int newNode = edge[0], newDist = edge[1];
+
+                if(map.containsKey(newNode)) {
+                    minHeap.add(new int[]{ topDist + newDist, newNode });
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            map.putIfAbsent(i, -1);
+        }
+
+        return map;
+    } 
 }
